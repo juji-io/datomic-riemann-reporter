be24aa85deed4531857b80b071d8c8dab1041218
Huahai Yang <huahai.yang@gmail.com>
2020-05-05 05:29:44 -0700

    update deps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject juji/datomic-riemann-reporter "0.2.1"
+(defproject juji/datomic-riemann-reporter "0.2.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [riemann-clojure-client "0.4.5"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [riemann-clojure-client "0.5.1"]
                  [environ "1.1.0"]])
