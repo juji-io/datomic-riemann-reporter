4866b22afc08e36e09ea418713f42d7eccbdb0d8
Huahai Yang <huahai.yang@gmail.com>
2020-05-21 07:21:01 -0700

    revert netty pinning

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject juji/datomic-riemann-reporter "0.2.3"
+(defproject juji/datomic-riemann-reporter "0.2.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.netty/netty-all "4.1.32.Final"]
                  [riemann-clojure-client "0.5.1"]
                  [environ "1.2.0"]])
